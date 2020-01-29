c9a3b6cf2cafbca7cf67dfac733cc179f4fee3aa
melonfruity <eric.luo24@gmail.com>
2020-01-28 23:04:01 -0500

    setting up react eslint

---
+++ client/src/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+      "browser": true,
+      "node": true
+  },
+  "extends": [
+    "airbnb",
+    "airbnb/hooks"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }],
+    "jsx-a11y/label-has-associated-control": [ "error", {
+      "required": {
+        "some": [ "nesting", "id"  ]
+      }
+    }],
+    "jsx-a11y/label-has-for": [ "error", {
+      "required": {
+        "some": [ "nesting", "id"  ]
+      }
+    }],
+    "react/jsx-props-no-spreading": "off",
+  } 
+}
