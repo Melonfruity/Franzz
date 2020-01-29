feba8cd0a0ed10dd73843f7b127f20193e580f86
melonfruity <eric.luo24@gmail.com>
2020-01-29 17:47:04 -0500

    stuck on value not updating

--- client/src/components/Channel/Container/Channel.js
+++ client/src/components/Channel/Container/Channel.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Chatbox from './Chat/Chatbox';
+import Canvas from './Canvas/Canvas';
 
 const Channel = () => {
   return (
     <div>
       Channel
       <Chatbox />
+      <Canvas />
     </div>
   );
 };
