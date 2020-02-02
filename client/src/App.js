41081fa71b3d8e82e3e7ad78912d21333604ee60
Julie Nguyen <julie-nguyen@live.com>
2020-02-02 00:22:31 -0500

    created button for window

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Channel from './components/Channel/Container/Channel';
 import PopUpBox from './components/Channel/Container/Chat/PopUpBox';
+import PopUpButton from './components/Channel/Container/PopUpButtons/PopUpButton'
 import "./components/Channel/Container/Chat/Styling/PopUpBoxStyling.css";
 import { mouseDownFunction } from './components/Channel/Container/Chat/Scripts/PopUpBoxScript';
 
+const ON = "on";
+const OFF = "off";
 
 const App = () => {
   const title = 'JEK';
-
 
   return (
     <div>
       {title}
       <Channel />
       <PopUpBox mouseDown={mouseDownFunction}/>
+      <PopUpButton />
     </div>
   );
 };
---
+++ client/src/components/Channel/Container/PopUpButtons/PopUpButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PopUpButton(props) {
+  return (
+    <button>
+      Click me for toggling window
+    </button>
+  )
+}
