5c5b5258410f3bfeaa6a80cb2158346a14e2413c
melonfruity <eric.luo24@gmail.com>
2020-02-01 23:50:02 -0500

    login page, uses react google auth now

--- client/src/components/Landing/Login.js
+++ client/src/components/Landing/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 import { GoogleLogin } from 'react-google-login';
 import { GOOGLE_CLIENT_ID } from '../../utils/config';
 
 const Login = () => {
-  const googleResponse = (res) => {
-    console.log(res)
+  const googleResponse = (data) => {
+    console.log(data);
+    axios
+      .post('http://localhost:8001/api/auth/google', data)
+      .then((res) => console.log(res));
   };
 
   const onFailure = (err) => {
