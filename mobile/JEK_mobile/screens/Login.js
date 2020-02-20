3248c1c41952080a3e90f6338ba8b1508a30cb72
melonfruity <eric.luo24@gmail.com>
2020-02-20 18:54:08 -0500

    connected to server

---
+++ mobile/JEK_mobile/screens/Login.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text, TextInput, SafeAreaView } from 'react-native';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+
+  return (
+    <View>
+      <Text style={styles.text}>Select a Username</Text>
+      <TextInput
+        style={styles.text}
+        placeholder='username'
+        onChangeText={(val) => setUsername(val)}
+        value={username}
+      />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderColor: '#777',
+    borderWidth: 1,
+    padding: 8,
+    margin: 10,
+    width: 200,
+    maxHeight: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default Login;
