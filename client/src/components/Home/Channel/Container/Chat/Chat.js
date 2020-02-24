e1eabe6f35a8dc44d93e1353c5ca87fa79826005
melonfruity <eric.luo24@gmail.com>
2020-02-24 18:09:45 -0500

    emoji picker

--- client/src/components/Home/Channel/Container/Chat/Chat.js
+++ client/src/components/Home/Channel/Container/Chat/Chat.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
 import TextContainer from './TextContainer';
 import Input from './Input';
 import ImageBox from '../../Modules/Photos/ImageBox';
@@ -6,64 +7,78 @@ import StalkerMap from '../../Modules/StalkerMap/StalkerMap';
 import YoutubeSync from '../../Modules/VideoSync/YoutubeSync';
 import Canvas from '../../Modules/Canvas/Canvas';
 
-
 import './Styling/DragAndDropBox.css';
 import './Chat.css';
 
 const Chat = ({
   messages, emitDeleteMessage, emitSendMessage, channel, currentUser, moduleView, locations, center, videoStates, changeVideoState, syncVideo, line, state, setState,
-}) => (
-  <div id="chat-container">
-    <TextContainer
-      messages={messages}
-      emitDeleteMessage={emitDeleteMessage}
-      emitSendMessage={emitSendMessage}
-      channelId={channel}
-      currentUser={currentUser}
-    />
-    <Input
-      emitSendMessage={emitSendMessage}
-      channel={channel}
-    />
-    { moduleView.imageBox && (
-      <ImageBox
-        channelId={channel}
-        emitSendMessage={emitSendMessage}
-      />
-    )}
-    { moduleView.stalkerMap && (
-    <StalkerMap
-      locations={locations}
-      channel={channel}
-      center={center}
-    />
-    )}
-    { moduleView.video && (
-    <YoutubeSync
-      channel={channel}
-      videoStates={videoStates}
-      changeVideoState={changeVideoState}
-      viewState={moduleView.video}
-      syncVideo={syncVideo}
-    />
-    )}
-    { moduleView.canvasBox && (
-      <div className="canvas resize-box">
-        {/* <h3 style={{ textAlign: 'center' }}></h3> */}
-        <div style={{ display: 'flex' }} className="main">
-          <div style={{ width: '225px' }} className="left-guide" />
-          <Canvas
-            className="canvasBoard"
-            line={line}
-            setState={setState}
-            state={state}
-            channel={channel}
-          />
-          <div style={{ width: '185px' }} className="right-guide" />
-        </div>
-      </div>
-    )}
-  </div>
-);
+}) => {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [message, setMessage] = useState('');
 
+  const onEmojiClick = (event, emojiObject) => {
+    event.preventDefault();
+    setMessage((prev) => prev.concat(emojiObject.emoji));
+  };
+
+  return (
+    <div id="chat-container">
+      <TextContainer
+        messages={messages}
+        emitDeleteMessage={emitDeleteMessage}
+        emitSendMessage={emitSendMessage}
+        channelId={channel}
+        currentUser={currentUser}
+      />
+      <Input
+        setShowEmojiPicker={setShowEmojiPicker}
+        emitSendMessage={emitSendMessage}
+        channel={channel}
+        message={message}
+        setMessage={setMessage}
+      />
+      { moduleView.imageBox && (
+        <ImageBox
+          channelId={channel}
+          emitSendMessage={emitSendMessage}
+        />
+      )}
+      { moduleView.stalkerMap && (
+      <StalkerMap
+        locations={locations}
+        channel={channel}
+        center={center}
+      />
+      )}
+      { moduleView.video && (
+      <YoutubeSync
+        channel={channel}
+        videoStates={videoStates}
+        changeVideoState={changeVideoState}
+        viewState={moduleView.video}
+        syncVideo={syncVideo}
+      />
+      )}
+      { moduleView.canvasBox && (
+        <div className="canvas resize-box">
+          {/* <h3 style={{ textAlign: 'center' }}></h3> */}
+          <div style={{ display: 'flex' }} className="main">
+            <div style={{ width: '225px' }} className="left-guide" />
+            <Canvas
+              className="canvasBoard"
+              line={line}
+              setState={setState}
+              state={state}
+              channel={channel}
+            />
+            <div style={{ width: '185px' }} className="right-guide" />
+          </div>
+        </div>
+      )}
+      { showEmojiPicker && (<div className="emojiPicker"><Picker onEmojiClick={onEmojiClick} /></div>)}
+    </div>
+  );
+};
+
+// onEmojiClick
 export default Chat;
